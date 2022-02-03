22547dc43ca4ff8a9ad0be48fbe246d430f366fb
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-02-03 14:45:20 -0600

    added index, js/. updated partner-bio

---
+++ js/my-first-js-file.js
@@ -0,0 +1,17 @@
+"use strict";
+
+//console.log("This is fine.");
+
+
+
+let myName = prompt(message: "Hi, what's your first name?");
+
+
+myName += " " + prompt(message: "What is your last name, good sir?");
+
+//console.log(myName.toUpperCase());
+
+//if (confirm("Are you awesome today?")){
+//    console.log("Always good to hear!");
+//}
+
