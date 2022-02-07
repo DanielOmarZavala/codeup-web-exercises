240ed2d65547e2d3522fdf328ce584525bc20fd4
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-02-07 13:58:07 -0600

    updated js/if-elseif-else-exercise.js improved line 14

--- js/if-elseif-else-exercise.js
+++ js/if-elseif-else-exercise.js
@@ -11,11 +11,11 @@ function canGetRideshare(driverNear, enoughFunds){
 function getToDestination(age, isInsured, hasCar){
     if (age > 16 && isInsured && hasCar){
         console.log("You can use your own vehicle.");
-    }else if ((age <= 16 && !isInsured || !hasCar) && canGetRideshare(true, true)){
+    }else if (age <= 16 && (!isInsured || !hasCar) && canGetRideshare(true, true)){
         console.log("You can call up a rideshare!");
     }else{
         console.log("Sorry, try calling a friend for a ride!");
     }
 }
 
-getToDestination(16, false, true);
+getToDestination(16, false, false);
