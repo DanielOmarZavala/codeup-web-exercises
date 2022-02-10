725c6f0b5c0ea4e6c1f0570918cc855ae69fc85b
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-02-10 13:17:36 -0600

    updated js/for_loops.js completed exercise 3

--- js/for_loops.js
+++ js/for_loops.js
@@ -12,5 +12,30 @@
 // }
 
 //Random Odd/Even Console.log - For Loop
+// function isOddOrEven (numIn) {
+//     if (numIn % 2 === 0) {
+//         console.log(numIn + " is even!");
+//     } else if (numIn % 2 !== 0) {
+//         console.log(numIn + " is odd!");
+//     } else {
+//         console.log("Something went wrong...");
+//     }
+// }
+//
+// for (let i = 0; i < 10; i++) {
+//     var numPool = Math.floor(Math.random() * 200) + 20;
+//
+//     isOddOrEven(numPool);
+// }
+
+//Number pseudo number pyramid
+for (let i = 1; i <= 9; i++) {
+    let sameLine = '';
+
+    for (let j = i; j > 0; j--) {
+        sameLine += i + ' ';
+    }
+    console.log(sameLine);
+}
 
 
