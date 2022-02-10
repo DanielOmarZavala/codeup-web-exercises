46ba6154acdee17d0579aa85303025da9c3e5f34
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-02-10 16:02:38 -0600

    updated js/break_and_continue.js finished the exercise.

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,5 +1,15 @@
 "use strict";
 
-do {
-    let numIn = prompt("Please enter an odd number between '1' and '50.'");
-} while (typeof numIn !== typeof 1)
+let numIn = 0;
+
+while (numIn % 2 === 0) {
+    numIn += parseInt(prompt("Please enter an odd number between '1' and '50.'"));
+}
+
+for (let i = 0; i <= 50; i++) {
+    if (i === numIn) {
+        console.log("Yikes! Skipping number: " + i);
+    } else if (i % 2 !== 0) {
+        console.log("Here is an odd number: " + i);
+    }
+}
