fd158b63be2ed0b1afc5827e6217a4d138107607
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-02-11 15:44:43 -0600

    updated js/break_and_continue.js added DocRob's Example

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//My Version
 let numIn = 0;
 
 while (numIn % 2 === 0 || numIn > 50 || numIn < 0) {
@@ -13,3 +14,34 @@ for (let i = 0; i <= 50; i++) {
         console.log("Here is an odd number: " + i);
     }
 }
+
+//DocRob Example
+// var oddNumber = promptForOddNumberInRange(1, 50);
+// oddNumber = parseInt(oddNumber);
+//
+// printOddNumbersInRangeWithSkip(1, 50, oddNumber);
+//
+// function printOddNumbersInRangeWithSkip(low, high, skipNumber) {
+//     for (let i = low; i <= high; i++) {
+//         if(i === skipNumber || i % 2 === 0) {
+//             if(i=== skipNumber) {
+//                 console.log("Yikes! Skipping number: " + i);
+//             }
+//             continue;
+//         }
+//         console.log("Here is an odd number: " + i);
+//     }
+// }
+//
+// function promptForOddNumberInRange(low, high) {
+//     //loop until user gives an odd number in the range INCLUSIVE
+//     var result;
+//     while(true) {
+//         result = prompt("Enter an odd number from " + low " to " + high);
+//
+//         if(result % 2 === 1 && result >= low && result <= high) {
+//             break;
+//         }
+//     }
+//     return result;
+// }
