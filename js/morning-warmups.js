6a1ca850e91bab291c4c2c50651fe8930ff2b897
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-02-16 12:39:41 -0600

    updated js/morning-warmups.js refactored lines 51+

--- js/morning-warmups.js
+++ js/morning-warmups.js
@@ -50,36 +50,38 @@ let dt = new Date().toString(); with NO console.log and NO .split WILL RETURN Mo
 
 /*CONSTRUCTOR FUNCTION*/
 
-// Defines a contact info object
-// function ContactInfo(phoneNumber, streetAddress, city, state, postalCode, emailAddress){
-//     this.phoneNumber = phoneNumber;
-//     this.streetAddress = streetAddress;
-//     this.city = city;
-//     this.state = state;
-//     this.postalCode = postalCode;
-//     this.emailAddress = emailAddress;
-// }
-// Defines a person object
-// function Person(names, occupation, dob, contactInfo) {
-//     this.names = names;
-//     this.occupation = occupation;
-//     this.dob = dob;
-//     this.contactInfo = contactInfo;
-// }
-// makes a new person object instance (by invoking the constructor)
-// let person2 = new Person(
-//     ["Casey", "J", "Edwards"],
-//     "Cat Herder / Firefighter",
-//     "11.30.1989",
-//     new ContactInfo(
-//         '123.456.7890',
-//         '123 Anywhere Ln',
-//         'Beverly Hills',
-//         'CA',
-//         '90210',
-//         'nope@no-no-no.no'));
+/*Defines a contact info object*/
+function ContactInfo(phoneNumber, streetAddress, city, state, postalCode, emailAddress){
+    this.phoneNumber = phoneNumber;
+    this.streetAddress = streetAddress;
+    this.city = city;
+    this.state = state;
+    this.postalCode = postalCode;
+    this.emailAddress = emailAddress;
+}
 
-// console.log(person2.contactInfo.emailAddress);
+/*Defines a person object*/
+function Person(names, occupation, dob, contactInfo) {
+    this.names = names;
+    this.occupation = occupation;
+    this.dob = dob;
+    this.contactInfo = contactInfo;
+}
+
+/*makes a new person object instance (by invoking the constructor)*/
+let person2 = new Person(
+    ["Casey", "J", "Edwards"],
+    "Cat Herder / Firefighter",
+    "11.30.1989",
+    new ContactInfo(
+        '123.456.7890',
+        '123 Anywhere Ln',
+        'Beverly Hills',
+        'CA',
+        '90210',
+        'nope@no-no-no.no'));
+
+console.log(person2.contactInfo.emailAddress);
 
 // let randomNum = Math.random();
 // console.log(randomNum);
@@ -88,14 +90,3 @@ let dt = new Date().toString(); with NO console.log and NO .split WILL RETURN Mo
 // let rangedNum = Math.floor((randomNum * (100 - 20) + 1)) + 20;
 // console.log(rangedNum);
 
-
-var circle = {
-    radius: 3,
-    getArea: function () {
-        // TODO: complete this method
-
-        return Math.PI * circle.radius ^ 2; // TODO: return the proper value
-    }
-};
-
-console.log(circle.getArea());
