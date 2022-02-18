ded4d4596e945cf8b8336e7241117103bad6d18b
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-02-18 09:13:45 -0600

    updated js/morning-warmups.js added warmup 5

--- js/morning-warmups.js
+++ js/morning-warmups.js
@@ -105,57 +105,66 @@ Should this be a standalone function or should it be a method on the Person obje
 * */
 
 
-// Defines a contact info object
-function ContactInfo(phoneNumber, streetAddress, city, state, postalCode, emailAddress) {
-    this.phoneNumber = phoneNumber;
-    this.streetAddress = streetAddress;
-    this.city = city;
-    this.state = state;
-    this.postalCode = postalCode;
-    this.emailAddress = emailAddress;
+// // Defines a contact info object
+// function ContactInfo(phoneNumber, streetAddress, city, state, postalCode, emailAddress) {
+//     this.phoneNumber = phoneNumber;
+//     this.streetAddress = streetAddress;
+//     this.city = city;
+//     this.state = state;
+//     this.postalCode = postalCode;
+//     this.emailAddress = emailAddress;
+// }
+//
+// // Defines a person object
+// function Person(occupation, dob, contactInfo) {
+//
+//     // assign a property the value of a function *definition*
+//     // this helps get the names of a person, but is not executed until you invoke getNames
+//     this.getNames = getNames;
+//     // actually *invoke* this.getNames() so the names are given when the Person object is created
+//     this.names = this.getNames();
+//
+//     this.occupation = occupation;
+//     this.dob = dob;
+//     this.contactInfo = contactInfo;
+// }
+//
+// // This function helps define *how* a user gives their names and returns
+// // those names as a string array
+// function getNames() {
+//     let namesArr = [];
+//     let hasMoreNames = true;
+//     while (hasMoreNames) {
+//         // get those names!
+//         namesArr.push(prompt("Please enter your desired name"));
+//
+//         // if they want more names, keep looping. if not, break the loop
+//         hasMoreNames = confirm("Would you like to enter another name?");
+//     }
+//     return namesArr;
+// }
+//
+// // Make new Person object
+// let person2 = new Person(
+//     "Cat Herder / Firefighter",
+//     "11.30.1989",
+//     new ContactInfo(
+//         '123.456.7890',
+//         '123 Anywhere Ln',
+//         'Beverly Hills',
+//         'CA',
+//         '90210',
+//         'nope@no-no-no.no')
+// );
+//
+// // Log the object so we can see its values
+// console.log(person2);
+
+
+
+/*Warmup: Create a function which returns true if the given argument is a number, false if it is not a number*/
+function isNum (valIn) {
+    return typeof valIn === typeof 2;
 }
 
-// Defines a person object
-function Person(occupation, dob, contactInfo) {
-
-    // assign a property the value of a function *definition*
-    // this helps get the names of a person, but is not executed until you invoke getNames
-    this.getNames = getNames;
-    // actually *invoke* this.getNames() so the names are given when the Person object is created
-    this.names = this.getNames();
-
-    this.occupation = occupation;
-    this.dob = dob;
-    this.contactInfo = contactInfo;
-}
-
-// This function helps define *how* a user gives their names and returns
-// those names as a string array
-function getNames() {
-    let namesArr = [];
-    let hasMoreNames = true;
-    while (hasMoreNames) {
-        // get those names!
-        namesArr.push(prompt("Please enter your desired name"));
-
-        // if they want more names, keep looping. if not, break the loop
-        hasMoreNames = confirm("Would you like to enter another name?");
-    }
-    return namesArr;
-}
-
-// Make new Person object
-let person2 = new Person(
-    "Cat Herder / Firefighter",
-    "11.30.1989",
-    new ContactInfo(
-        '123.456.7890',
-        '123 Anywhere Ln',
-        'Beverly Hills',
-        'CA',
-        '90210',
-        'nope@no-no-no.no')
-);
-
-// Log the object so we can see its values
-console.log(person2);
+console.log(isNum("3"));
