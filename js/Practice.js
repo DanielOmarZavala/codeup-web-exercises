443e0739b786d6d414a9b5a8fd198c407676224a
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-02-25 19:39:47 -0600

    added js/Practice.js

---
+++ js/Practice.js
@@ -0,0 +1,18 @@
+"use strict"
+
+console.log("test file");
+
+let customer = {
+    wallet: 0,
+    status: function {
+        if () {
+
+        }
+    };
+};
+
+function status (statusIn) {
+
+}
+
+let statusIn = prompt("What is your status?");
