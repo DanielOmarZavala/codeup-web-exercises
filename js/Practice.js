90411ee29cc4bbdec50a731053564b677b22d0d9
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-02-28 09:33:28 -0600

    morning JS warmups

--- js/Practice.js
+++ js/Practice.js
@@ -1,3 +1,4 @@
+/*
 "use strict"
 
 console.log("test file");
@@ -15,4 +16,38 @@ function status (statusIn) {
 
 }
 
-let statusIn = prompt("What is your status?");
+let statusIn = prompt("What is your status?");*/
+
+// var bugs = ["mosquito", "ant", "scorpion", "ant", "ant", "mosquito", "typo", "reference error", "type error"];
+//
+// function removeAll (arrIn, remove) {
+//     return arrIn.pop(remove);
+// }
+//
+// console.log(removeAll(bugs, "ant"));
+
+
+// should print
+// [
+//     'mosquito',
+//     'scorpion',
+//     'mosquito',
+//     'typo',
+//     'reference error',
+//     'type error'
+// ]
+
+var bugs = ["mosquito", "ant", "scorpion", "ant", "ant", "mosquito", "typo", "reference error", "type error"];
+
+function removeAll(anArr,valueToRemove) {
+    let filteredArr = [];
+    anArr.forEach(function(ele) {
+        if (valueToRemove !== ele) {
+            filteredArr.push(ele);
+        }
+    });
+    return filteredArr;
+}
+
+console.log(removeAll(bugs, "ant"));
+
