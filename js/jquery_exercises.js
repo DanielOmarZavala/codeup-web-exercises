b3f61c845638f0c995a3d156831f59df958a69e0
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-03-11 10:49:30 -0600

    finished the exercise

---
+++ js/jquery_exercises.js
@@ -0,0 +1,15 @@
+"use strict"
+
+$(document).ready(function() {
+    alert( 'The DOM has finished loading!' );
+});
+
+
+//     $("nav-bar").css("background-color", "green");
+//
+//     $("div.container-fluid > p.col-12").hover(function() {
+//     //hover-in function
+//     $(this).css("background-color", "lightblue");
+// }, function() {
+//     $(this).css("background-color", "none");
+// });
