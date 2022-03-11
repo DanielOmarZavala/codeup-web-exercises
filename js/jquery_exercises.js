9d4fe87a24adc277e009632a2366b527cb858e9b
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-03-11 12:45:37 -0600

    jquery_exercises.html js/jquery_exercises.js refactored to write html in .js

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -4,6 +4,18 @@
 //     alert( 'The DOM has finished loading!' );
 // });
 
+$("body").append(getTemplate());
+
+function getTemplate () {
+    return '<h1 id="top-heading" class="heading">jquery</h1>' +
+        '<p id="p-tag" class="p-tag">' +
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci amet dolor dolore eligendi esse eum exercitationem rem sit voluptatum.' +
+        '</p>' +
+        '<ul id="ul-element" class="ul-element">' +
+        '<li id="li-element" class="li-element">li element</li>' +
+        '</ul>' +
+        '<div id="div-element" class="div-element">div element</div>';
+}
 
 let topHeading = $("#top-heading").html();
 alert(topHeading);
