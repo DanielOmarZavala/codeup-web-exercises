b312dad10a9d4540ee61dd22b8b9852a0cac84e4
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-03-12 12:41:17 -0600

    completed some practice work

--- js/Practice.js
+++ js/Practice.js
@@ -21,9 +21,9 @@
 
 
 /*FUNCTION DEFINITION*/
-let handler = function (event) {
-    alert("You clicked something!");
-}
+// let handler = function (event) {
+//     alert("You clicked something!");
+// }
 
 /*FUNCTION EXPRESSION ONLY ACCESSIBLE BELOW FUNCTION*/
 // document.addEventListener("click", handler);
@@ -35,18 +35,18 @@ let handler = function (event) {
 
 /*THREE STEPS OF EVEN LISTENERS*/
 /*ACCES (GRAB) ELEMENT*/
-let header = document.querySelector("#main-header");
+// let header = document.querySelector("#main-header");
 
 /*ATTACH EVENT LISTENER*/
-header.addEventListener("click", headerClick);
-
-// WHAT HAPPENS ON HEADER CLICK
-function headerClick(event) {
-    header.style.backgroundColor = "yellow";
-}
-
-
-let input = document.querySelector("#search");
+// header.addEventListener("click", headerClick);
+//
+// // WHAT HAPPENS ON HEADER CLICK
+// function headerClick(event) {
+//     header.style.backgroundColor = "yellow";
+// }
+//
+//
+// let input = document.querySelector("#search");
 
 // input.addEventListener("keyup", function (event) {
 //     // PRINT ELEMENT
@@ -56,25 +56,52 @@ let input = document.querySelector("#search");
 //     console.log(event.code);
 // });
 
-input.addEventListener("keyup", upperCaseTextInput)
+// input.addEventListener("keyup", upperCaseTextInput)
+//
+// function upperCaseTextInput(event) {
+//     console.log(event.target.value);
+//     event.target.value = event.target.value.toUpperCase();
+// }
+//
+//
+// let moreBtn = document.querySelector("#more");
+//
+// moreBtn.addEventListener("click", function (e) {
+//     let p = e.target.previousElementSibling
+//     if (p.classList.contains("hidden")) {
+//         p.classList.remove("hidden");
+//         e.target.innerText = "Less";
+//     } else {
+//         p.classList.add("hidden");
+//         e.target.innerText = "More";
+//     }
+// });
 
-function upperCaseTextInput(event) {
-    console.log(event.target.value);
-    event.target.value = event.target.value.toUpperCase();
-}
+// $('main div:first-child(1)').click(function() {
+//   let content = $(this).html();
+// $('main div:nth-child(3)').html(content);
+// });
 
 
-let moreBtn = document.querySelector("#more");
+// let originalColor;
+/*$('.card').hover(function() {
 
-moreBtn.addEventListener("click", function (e) {
-    let p = e.target.previousElementSibling
-    if (p.classList.contains("hidden")) {
-        p.classList.remove("hidden");
-        e.target.innerText = "Less";
-    } else {
-        p.classList.add("hidden");
-        e.target.innerText = "More";
-    }
+    //2. apply classes/styles to hovered element
+    // $(this).css('background-color', 'lightblue');
+    $(this).toggleClass('bg-dark');
+    $(this).toggleClass('text-light');
+
+
+}, function() {
+
+    // $(this).css('background-color', originalColor);
+    $(this).toggleClass('bg-dark');
+    $(this).toggleClass('text-light');
+});*/
+
+$("#user-input").keyup(function(e) {
+
+    let content = $(this).val();
+
+    $("#user-output").text(content);
 });
-
-
