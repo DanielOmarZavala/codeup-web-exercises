7173a5d81e2107ba10722f2f07947c6029ab8d3e
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-03-15 16:28:39 -0500

    working on starwars api

--- js/keys.js
+++ js/keys.js
@@ -1,3 +1,6 @@
 
 
-const OWM_KEY = "6226d6209a3ba704a642eee713129bea";
+// const OWM_KEY = "6226d6209a3ba704a642eee713129bea";
+
+const OWM_KEY = "5d59820598mshe8bc1caa80ed252p1685e6jsned1b60a8e2d2";
+const HOST = "league-of-legends-champions.p.rapidapi.com"
