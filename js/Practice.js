dceb2431f26ee17e5908d307224a7defd7b5ad29
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-03-15 10:17:37 -0500

    updated js/jquery_faq.js cleaned up comments

--- js/Practice.js
+++ js/Practice.js
@@ -105,3 +105,26 @@ $("#user-input").keyup(function(e) {
 
     $("#user-output").text(content);
 });
+
+// $('body').append(getNewParagraph());
+// $('body').insertBefore(getNewParagraph());
+// $('body > script').first().before($(getNewParagraph()).addClass('test-class'));
+
+//log out the text contents of EACH selected element
+// $('body > div > p').each(function(){
+//     console.log($(this).text());
+// });
+//
+// $('ul > li').each(function(){
+//     if ($(this).first().text() === 'Global Domination') {
+//         $(this).first().text('Global.. benevolence...')
+//     }
+// });
+//
+//
+//
+// //language=HTML
+// function getNewParagraph() {
+//     return '<div><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. ' +
+//         'Aspernatur corporis eos fuga iure laudantium nam nisi totam ut veniam vitae!</p></div>'
+// }
--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,28 +1,5 @@
 "use strict"
 
-// $('body').append(getNewParagraph());
-// $('body').insertBefore(getNewParagraph());
-// $('body > script').first().before($(getNewParagraph()).addClass('test-class'));
-
-//log out the text contents of EACH selected element
-// $('body > div > p').each(function(){
-//     console.log($(this).text());
-// });
-//
-// $('ul > li').each(function(){
-//     if ($(this).first().text() === 'Global Domination') {
-//         $(this).first().text('Global.. benevolence...')
-//     }
-// });
-//
-//
-//
-// //language=HTML
-// function getNewParagraph() {
-//     return '<div><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. ' +
-//         'Aspernatur corporis eos fuga iure laudantium nam nisi totam ut veniam vitae!</p></div>'
-// }
-
 $(document).ready(function (){
 
 
