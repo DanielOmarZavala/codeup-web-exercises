b7c12ce7620bbd63c3732343c4e13a372d360d3d
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-03-29 14:04:26 -0500

    added promises.html, js/promises.js and started the exercise

---
+++ js/promises.js
@@ -0,0 +1,26 @@
+"use strict"
+
+/* Main Fetch Function */
+function getUserLastPushEvent(username) {
+
+let url = `https://api.github.com/users/${username}/events/public`;
+// let url = `https://api.github.com/repos/danielomarzavala/codeup-web-exercises/commits`;
+
+fetch(url, {headers: {'Authorization': GIT_KEY}})
+    .then(response => response.json())
+    .then(response => coolFunc(response))
+    .catch(err => console.error(err));
+}
+
+/* Invoke Fetch Function and Populate on DOM */
+getUserLastPushEvent('mojombo').then(event => {
+    $('body').innerHTML('<h1>' + 'Mojombos last push event was created at' +  event.created_at + '</h1>')
+});
+
+/* Filter to Push Event */
+function coolFunc (response) {
+    for (let event of response) {
+        if (event.type === 'PushEvent')
+            return event;
+    }
+}
